Fixed normalisation in sl_loglik. Finished evaluation script.

noised_AR_pred.py
@@ -1,33 +1,78 @@
 import lab as B
+from neuralprocesses.aggregate import Aggregate, AggregateInput
 import neuralprocesses.torch as nps
 import numpy as np
 import torch
 
+from neuralprocesses.dist.normal import MultiOutputNormal
+from mask_context import mask_context
+
 __all__ = ["generate_AR_prediction"]
 
-def generate_AR_prediction(state, model, gen, num_samples):
+
+def generate_AR_prediction(state, model, gen, num_samples, normalise=True):
+
+    gen.batch_size = 1
+    batch = gen.generate_batch()
+    true_y0t = list(batch["yt"])[0]
+    float = B.dtype_float(true_y0t)
+    float64 = B.promote_dtypes(float, np.float64)
 
     with torch.no_grad():
 
+        logpdfs = None
         for _ in range(num_samples):
 
-            batch = gen.generate_batch()
-            print(batch["contexts"])
+            # Generating predictions for y2t
+            contexts = mask_context(batch["contexts"], 1, 2)
+            state, _, _, _, yt = nps.predict(state,
+                                             model,
+                                             contexts,
+                                             batch["xt"],
+                                             num_samples=1,
+                                             batch_size=1)
+            y2t_pred = list(yt)[2].squeeze(0).float()
 
-        #     state, pred = model(
-        #     state,
-        #     contexts,
-        #     xt,
-        #     num_samples=this_num_samples,
-        #     dtype_enc_sample=float,
-        #     dtype_lik=float64,
-        #     **kw_args,
-        # )
-        # pred = fix_noise_in_pred(pred, fix_noise)
+            # Generating predictions for y1t
+            contexts[2] = (list(batch["xt"])[0][0], y2t_pred)
+            state, _, _, _, yt = nps.predict(state,
+                                             model,
+                                             contexts,
+                                             batch["xt"],
+                                             num_samples=1,
+                                             batch_size=1)
+            y1t_pred = list(yt)[1].squeeze(0).float()
 
+            # Generating predictions for y0t
+            contexts[1] = (list(batch["xt"])[0][0], y1t_pred)
+            # state, _, _, _, yt = nps.predict(state,
+            #                                  model,
+            #                                  contexts,
+            #                                  batch["xt"],
+            #                                  num_samples=1,
+            #                                  batch_size=1)
+            # y0t_pred = list(yt)[0].squeeze(0)
+            state, pred = model(
+                state,
+                contexts,
+                batch["xt"],
+            )
+            mean = list(pred.mean)[0].squeeze(2)
+            var = list(pred.var)[0].squeeze(2)
+            shape = list(pred.shape)[0]
+            SL_pred = MultiOutputNormal.diagonal(mean, var, shape)
+            this_logpdfs = SL_pred.logpdf(B.cast(float64, true_y0t))
 
-            print(_)
-            loglik=0
+            if logpdfs is None:
+                logpdfs = this_logpdfs
+            else:
+                logpdfs = B.concat(logpdfs, this_logpdfs, axis=0)
 
-    return state, loglik
+        # Average over samples.
+        logpdfs = B.logsumexp(logpdfs, axis=0) - B.log(num_samples)
 
+        if normalise:
+            # Normalise by the number of targets.
+            logpdfs = logpdfs / B.cast(float64, nps.num_data(AggregateInput(batch["xt"][0]), Aggregate(batch["yt"][0])))
+
+    return state, logpdfs

train_noised.py
@@ -13,27 +13,12 @@ import torch
 import wbml.out as out
 from matrix.util import ToDenseWarning
 from wbml.experiment import WorkingDirectory
+from mask_context import mask_context
 from noised_AR_pred import generate_AR_prediction
 
-__all__ = ["main"]
+__all__ = ["main", "mask_context"]
 
 warnings.filterwarnings("ignore", category=ToDenseWarning)
-
-
-def _mask_context(context, level_index):
-    
-    if level_index == 0:
-        # If 0, no masking is carried out, and all three context sets are passed on.
-        return context
-    elif level_index == 1:
-        # If 1, y1t is masked, and only y2t and yc are passed as context
-        context[1] = (B.randn(torch.float32, 16, 1, 0), B.randn(torch.float32, 16, 1, 0))
-        return context
-    elif level_index == 2:
-        # If 2, y1t and y2t are masked, and only yc is passed as context
-        context[1] = (B.randn(torch.float32, 16, 1, 0), B.randn(torch.float32, 16, 1, 0))
-        context[2] = (B.randn(torch.float32, 16, 1, 0), B.randn(torch.float32, 16, 1, 0))
-        return context
     
 
 def train(state, model, opt, objective, gen, *, fix_noise):
@@ -44,7 +29,7 @@ def train(state, model, opt, objective, gen, *, fix_noise):
         state, obj = objective(
             state,
             model,
-            _mask_context(batch["contexts"], level_index),
+            mask_context(batch["contexts"], gen.batch_size, level_index),
             batch["xt"],
             batch["yt"],
             level_index,
@@ -72,7 +57,7 @@ def eval(state, model, objective, gen):
             state, obj = objective(
                 state,
                 model,
-                _mask_context(batch["contexts"], level_index),
+                mask_context(batch["contexts"], gen.batch_size, level_index),
                 batch["xt"],
                 batch["yt"],
                 level_index,
@@ -481,12 +466,19 @@ def main(**kw_args):
         #                     ),
         #                     gen,
         #                 )
-        gen_pred = gen(batch_size=1)
-        state, loglik = generate_AR_prediction(state, model, gen, num_samples=5)
 
-        # Sleep for ten seconds before exiting.
-        out.out("Finished evaluation. Sleeping for ten seconds before exiting.")
-        time.sleep(10)
+        # Evaluate different context sets
+        num_datasets = 10
+        logliks = []
+        for _ in range(num_datasets):
+            state, loglik = generate_AR_prediction(state, model, gen, num_samples=10)
+            logliks.append(loglik)
+        logliks = B.concat(*logliks)
+        out.kv("Loglik (E)", exp.with_err(logliks, and_lower=True))
+
+        # # Sleep for ten seconds before exiting.
+        # out.out("Finished evaluation. Sleeping for ten seconds before exiting.")
+        # time.sleep(10)
     else:
         # Perform training. First, check if we want to resume training.
         start = 0
@@ -579,4 +571,5 @@ def main(**kw_args):
 
 
 if __name__ == "__main__":
-    main(data="noised_sawtooth", dim_y=3, epochs=100, objective="sl_loglik", evaluate=True)
+    main(data="noised_sawtooth", dim_y=3, epochs=100, objective="sl_loglik")
+    # main(data="noised_sawtooth", dim_y=3, epochs=100, objective="sl_loglik", evaluate=True)